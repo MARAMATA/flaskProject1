12e31cc2660e21faa3c6682f14876f3a68a6e13f
Your Name <you@example.com>
2024-10-13 22:01:15 +0000

    Correction des migrations et mise à jour de la base de données

--- test_config.py
+++ test_config.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 from dotenv import load_dotenv
 
 # Charger les variables d'environnement depuis le fichier .env
@@ -12,6 +12,6 @@ class TestConfig:
                                         'postgresql://default_user:default_pass@localhost:5432/API_FLASK_TEST')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TESTING = True  # Active le mode test pour Flask
+    TESTING = True  # Active le mode test pour Flask"""
 
 
